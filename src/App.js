2e3f0fe8713164a76efd62bd5ae0f89ac57727e2
ayham-kattan <ayhamkattan1@gmail.com>
2022-02-07 00:53:01 +0200

    addingImage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 function App() {
+  const [MenuItem, setMenuItem] = useState(data);
+  const [categories, setCategories] = useState([]);
+
+
+  console.log(data);
   return (
     <div className="App">
+      <h1>Our Menu</h1>
+
 
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const menu = [
+  {
+    id: 1, title: 'buttermilk pancakes', price: 15.99, category: 'breakfast', img: './images/1.jpg',
+    desc:`I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2, title: 'diner double', price: 13.99, category: 'lunch', img: './images/2.jpg',
+    desc:`vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica
+    `,
+  },  
+  {
+    id: 3, title: 'godzilla milkshake', price: 6.99, category: 'shakes', img: './images/3.jpg',
+    desc:`ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral
+    `,
+  },
+  {
+    id: 4, title: 'country delight', price: 20.99, category: 'breakfast', img: './images/4.jpg',
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5, title: 'egg attack', price: 22.99, category: 'lunch', img: './images/5.jpg',
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6, title: 'oreo dream', price: 18.99, category: 'shakes', img: './images/6.jpg',
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7, title: 'bacon overflow', price: 8.99, category: 'breakfast', img: './images/7.jpg',
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+
+]
+export default menu;
